0b385e770a9c8ff580bc614f2c23c2f4cf3a80c7
dortana <info@dortana.com>
2026-03-08 15:09:39 +0100

    remove extra import

--- src/controllers/customers/index.ts
+++ src/controllers/customers/index.ts
@@ -1,7 +1,5 @@
-import { getTranslator } from "@/utils/i18nContext";
 import type { Request, Response } from "express";
 import prisma from "@/config/db";
-import logger from "@/utils/logger";
 
 export const getCustomers = async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
