7597a072319d73dec743db1237d593677154cc36
Milad Seifi <milad.seifi@wolt.com>
2026-04-16 20:32:56 +0200

    set location

--- src/routes/customers/index.ts
+++ src/routes/customers/index.ts
@@ -1,4 +1,4 @@
-import { getCustomers } from "@/controllers/customers";
+import { getCustomers,setLocationHandler } from "@/controllers/customers";
 import { UserRole } from "@/generated/prisma/enums";
 import requiresAuth from "@/middlewares/auth";
 import { Router } from "express";
@@ -9,5 +9,7 @@ const router = Router();
 router.use(requiresAuth(UserRole.CUSTOMER));
 
 router.get("/test", getCustomers);
+
+router.put("/set-location", setLocationHandler);
 
 export default router;
